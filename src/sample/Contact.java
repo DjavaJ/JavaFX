fc46709b92b7ffef81a019d321fb0cf18acb597a
D'Angelo Joyce <dangelomjoyce@gmail.com>
2017-02-21 00:20:29 -0500

    JavaFXnew

---
+++ src/sample/Contact.java
@@ -0,0 +1,44 @@
+package sample;
+
+/**
+ * Created by dangelojoyce on 2/21/17.
+ */
+public class Contact {
+
+    String name;
+    String phoneNumber;
+
+    public Contact(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    String email;
+
+}
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import javax.swing.text.html.ListView;
+
+
 public class Controller {
+
+
+    @FXML
+    ListView setItem;
+
+    @FXML
+    TextField text;
+
+    public void addItem(){
+        
+    }
+
+
+
 }
